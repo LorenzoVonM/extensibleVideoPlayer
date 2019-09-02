ddeba4da480887c7f6d474dfdc843fb5b438dbde
Leonardo Flores <nadeshkokeane@hotmail.com>
2019-09-01 23:11:25 -0500

    commented plugin

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,11 @@ import MediaPlayer from "./MediaPlayer.js"
 import AutoPlay from "./plugins/AutoPlay.js"
 
 const video = document.querySelector('video');
-let conObj = { el: video, plugins: [new AutoPlay()] };
-const player = new MediaPlayer(conObj);
+let conObj = { el: video, 
+    plugins: [
+        //new AutoPlay()
+    ] };
+const player =new  MediaPlayer(conObj);
 
 const buttonPlay = document.querySelector('#btnPlayPause');
 const buttonMute = document.querySelector('#btnMute');
