6c2547162fa73456045083765956e9bc234cb8cd
Leonardo Flores <nadeshkokeane@hotmail.com>
2019-09-24 23:24:02 -0500

    function lesson in ts

--- exer/typescript/index.ts
+++ exer/typescript/index.ts
@@ -1,4 +1,25 @@
 console.log('hello typescript');
 
-let muted: boolean = true;
-muted= false
+function add(a: number, b: number){
+    return a + b;
+}
+
+const sum = add(6, 4);
+
+function createAdder(a: number): (number) => number{
+    return function(b: number) {
+        return b + a;
+    }
+}
+
+const addFour = createAdder(4)
+const fourPlusSix = (6);
+
+//function fullName(firstName: string,  lastName?: string): string FOR OPTIONAL VALUES
+function fullName(firstName: string,  lastName: string = 'sasda'): string {
+    return `${firstName} ${lastName}`;
+}
+
+const leo = fullName('leo');
+
+console.log(leo);
